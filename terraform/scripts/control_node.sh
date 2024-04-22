2499ffd21fbc5f133202b21a428b93ad46cbefd0
anvieira <anvieira@student.42porto.com>
2024-04-22 17:26:51 +0100

    update

--- terraform/scripts/control_node.sh
+++ terraform/scripts/control_node.sh
@@ -12,7 +12,6 @@ create_user() {
     chown -R $1:$1 /home/$1/.ssh || log_and_exit "Erro ao alterar o dono do diretório /home/$1/.ssh"
     usermod -aG sudo $1 || log_and_exit "Erro ao adicionar o usuário $1 ao grupo sudo"
     cat /tmp/public_keys/$1.pub | tee /home/$1/.ssh/authorized_keys > /dev/null || log_and_exit "Erro ao adicionar a chave pública ao arquivo /home/$1/.ssh/authorized_keys"
-    cp /tmp/ansible.pem /home/$1/.ssh/ansible.pem || log_and_exit "Erro ao copiar a chave privada para o usuário $1"
 }
 
 #users
@@ -46,12 +45,16 @@ mv /tmp/ansible /opt/ansible || log_and_exit "Erro ao mover o diretório /tmp/an
 groupadd ansible_3 || log_and_exit "Erro ao criar o grupo ansible"
 chown :ansible_3 /opt/ansible || log_and_exit "Erro ao alterar o grupo do diretório /opt/ansible"
 
+mv /tmp/ansible.pem /opt/key || log_and_exit "Erro ao mover o arquivo /tmp/ansible.pem para /opt/ansible.pem"
+chown -R root:ansible_3 /opt/key || log_and_exit "Erro ao alterar o dono do arquivo /opt/ansible.pem" 
+
 #visudo
 echo "%ansible_3 ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers || log_and_exit "Erro ao adicionar a permissão no arquivo /etc/sudoers"
 
-chmod 3770 /opt/ansible || log_and_exit "Erro ao alterar as permissões do diretório /opt/ansible"
+chmod -R 3770 /opt/ansible || log_and_exit "Erro ao alterar as permissões do diretório /opt/ansible"
 
 for nome in ${nomes[@]}; do
     usermod -aG ansible_3 $nome || log_and_exit "Erro ao adicionar o usuário $nome ao grupo ansible"
     ln -s /opt/ansible /home/$nome/ansible || log_and_exit "Erro ao criar o link simbólico /home/$nome/ansible"
+    ln -s /opt/key /home/$nome/ansible.pem || log_and_exit "Erro ao criar o link simbólico /home/$nome/ansible.pem"
 done
