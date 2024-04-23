04612675358beee9085f363ad6f95a42707e6337
anvieira <anvieira@student.42porto.com>
2024-04-23 19:10:18 +0100

    git in CN

--- terraform/scripts/control_node.sh
+++ terraform/scripts/control_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 log_and_exit() {
     #red
-    echo -e "\e[31m$1\e[0m" >> /tmp/control_node.logtm  
+    echo -e "\e[31m$1\e[0m" >> /tmp/control_node.log
     exit 1
 }
 
@@ -31,17 +31,9 @@ apt update -y || log_and_exit "Erro ao atualizar o repositório"
 apt install -y software-properties-common || log_and_exit "Erro ao instalar o pacote software-properties-common"
 apt-add-repository --yes --update ppa:ansible/ansible || log_and_exit "Erro ao adicionar o repositório do ansible"
 
-for package in ${package_to_install[@]}; do
-    apt install $package -y || log_and_exit "Erro ao instalar o pacote $package"
-done
 
-for package in ${package_to_upgrade[@]}; do
-    apt upgrade $package -y || log_and_exit "Erro ao atualizar o pacote $package"
-done
-
-pip install "pywinrm>=0.3.0" || log_and_exit "Erro ao instalar o pacote pywinrm"
-
-mv /tmp/ansible /opt/ansible || log_and_exit "Erro ao mover o diretório /tmp/ansible para /opt/ansible"
+git clone https://github.com/teroVF/ansible_project.git /opt/ansible || log_and_exit "Erro ao clonar o repositório ansible_project"l
+git config --global --add safe.directory /opt/ansible || log_and_exit "Erro ao adicionar o safe.directory"
 #Grupo ansible_3 com os usuários antero, miguel e pedro para rwx o diretório /opt/ansible
 groupadd ansible_3 || log_and_exit "Erro ao criar o grupo ansible"
 chown -R :ansible_3 /opt/ansible || log_and_exit "Erro ao alterar o grupo do diretório /opt/ansible"
@@ -51,12 +43,22 @@ chown -R root:ansible_3 /opt/ansible.pem || log_and_exit "Erro ao alterar o dono
 chown 600 /opt/ansible.pem || log_and_exit "Erro ao alterar as permissões do arquivo /opt/ansible.pem"
 
 #visudo
-echo "%ansible_3 ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers || log_and_exit "Erro ao adicionar a permissão no arquivo /etc/sudoers"
+echo '%ansible_3 ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers || log_and_exit "Erro ao adicionar a permissão no arquivo /etc/sudoers"
 
-chmod -R 3770 /opt/ansible || log_and_exit "Erro ao alterar as permissões do diretório /opt/ansible"
+chmod -R 770 /opt/ansible || log_and_exit "Erro ao alterar as permissões do diretório /opt/ansible"
 
 for nome in ${nomes[@]}; do
     usermod -aG ansible_3 $nome || log_and_exit "Erro ao adicionar o usuário $nome ao grupo ansible"
-    ln -s /opt/ansible /home/$nome/ansible || log_and_exit "Erro ao criar o link simbólico /home/$nome/ansible"
+    ln -s /opt/ansible/ansible_v1 /home/$nome/ansible || log_and_exit "Erro ao criar o link simbólico /home/$nome/ansible"
     ln -s /opt/ansible.pem /home/$nome/ansible.pem || log_and_exit "Erro ao criar o link simbólico /home/$nome/ansible.pem"
 done
+
+for package in ${package_to_install[@]}; do
+    apt install $package -y || log_and_exit "Erro ao instalar o pacote $package"
+done
+
+for package in ${package_to_upgrade[@]}; do
+    apt upgrade $package -y || log_and_exit "Erro ao atualizar o pacote $package"
+done
+
+pip install "pywinrm>=0.3.0" || log_and_exit "Erro ao instalar o pacote pywinrm"
